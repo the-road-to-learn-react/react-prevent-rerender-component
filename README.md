3d82ade77ae04a12f67792c7e7a1b97207340dc5
Robin Wieruch <wrobin@gmx.net>
2018-09-07 16:23:50 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# react-pure-component-example
+
+[![Build Status](https://travis-ci.org/the-road-to-learn-react/react-pure-component-example.svg?branch=master)](https://travis-ci.org/the-road-to-learn-react/react-pure-component-example) [![Slack](https://slack-the-road-to-learn-react.wieruch.com/badge.svg)](https://slack-the-road-to-learn-react.wieruch.com/)
+
+An application showcasing when and how to use shouldComponentUpdate or PureComponent in React for performance optimizations in update lifecycles (e.g. render). [Read more about it](https://www.robinwieruch.de/react-shouldcomponentupdate).
+
+## Features
+
+* performance optimization
+* React's shouldComponentUpdate
+* React's PureComponent
+* [recompose](https://github.com/acdlite/recompose)'s pure
+
+## Installation
+
+* `git clone git@github.com:the-road-to-learn-react/react-pure-component-example.git`
+* cd react-pure-component-example
+* npm install
+* npm start
+* visit `http://localhost:3000`
---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { Component, PureComponent } from 'react';
+import styled from 'styled-components';
+import { pure } from 'recompose';
+
+class App extends Component {
+  state = {
+    perspective: false,
+  };
+
+  togglePerspective = () => {
+    this.setState(state => ({ perspective: !state.perspective }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.togglePerspective}>
+          Toggle Perspective
+        </Button>
+
+        <Perspective perspective={this.state.perspective}>
+          {new Array(5000)
+            .fill(0)
+            .map((v, i) => <Exponential key={i} number={i} />)}
+        </Perspective>
+      </div>
+    );
+  }
+}
+
+const Perspective = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: ${props => (props.perspective ? 'row' : 'column')};
+`;
+
+const Button = ({ onClick, children }) => (
+  <button type="button" onClick={onClick}>
+    {children}
+  </button>
+);
+
+const Item = styled.div`
+  margin: 10px;
+`;
+
+const Exponential = pure(({ number }) => (
+  <Item>{number * number}</Item>
+));
+
+// FAST, no re-render, shallow shouldComponentUpdate check
+// class Exponential extends PureComponent {
+//   render() {
+//     return <Item>{this.props.number * this.props.number}</Item>;
+//   }
+// }
+
+// FAST, no re-render, fine-granular shouldComponentUpdate check
+// class Exponential extends Component {
+//   shouldComponentUpdate(nextProps, nextState) {
+//     if (this.props.number === nextProps.number) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+
+//   render() {
+//     return <Item>{this.props.number * this.props.number}</Item>;
+//   }
+// }
+
+// SLOW
+// class Exponential extends Component {
+//   render() {
+//     return <Item>{this.props.number * this.props.number}</Item>;
+//   }
+// }
+
+// SLOW
+// const Exponential = ({ number }) => <Item>{number * number}</Item>;
+
+export default App;
